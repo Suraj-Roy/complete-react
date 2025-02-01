1dac75e6a7ff7de35d3bebb1363e6a0d0d62be5d
Suraj <roysuraj365.5@gmail.com>
2025-02-01 13:34:22 +0530

    react implementation mannually V1.0

--- App.js
+++ App.js
@@ -3,11 +3,14 @@
     <h1>i am heading 2</h1>
 
 </div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 const containerdiv = React.createElement("div",{id:"container"},[
-    React.createElement("h1",null,["i am heading 1"]),
-        React.createElement("h1",null,["i am heading 2"]),
+    React.createElement("h1",{key:"1"},["i am heading 1"]),
+        React.createElement("h1",{key:"2"},["i am heading 2"]),
+        React.createElement("h1",{key:"3"},["i am heading 3"]),
 ])
 
 const root2 = ReactDOM.createRoot(document.getElementById("root"));
